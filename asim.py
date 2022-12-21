9306b4bb5e743d3f7dd3f2171aafcfec115ea7f0
Vangel Paronov <vparonov@gmail.com>
2022-12-21 21:44:43 +0100

    checkpoint

--- asim.py
+++ asim.py
@@ -23,54 +23,12 @@ def markAsPicked(stations):
     return g 
 
 def main():
-    print('------------------')
-    #simple_test()
     test()
-
-
-def simple_test():
-    nitems = 30
-    source = Source('source',delay=1, generator = genNitems(nitems))
-    c11 = Conveyor('c1.1',delay = 1, capacity =20)
-    s01 = Conveyor('s01',delay = 1, capacity = 10)
-    ds01 = Diverter('ds01',divertPredicate= lambda load: load.isForStationS('A'))
- 
-    sink = Sink('sink','sink')
-     
-    source.\
-        connect(c11).\
-        connect(ds01).\
-        connect(
-            straightConnection= sink, 
-            divertConnection= s01)
-    t = 1 
-    gotError = False
-    while True:
-        try:
-            source.tick(t)
-        except Exception as e:
-            gotError = True  
-            print(e)
-            source.print()
-            break
-        if t % 1000 == 0:
-            source.print()
-        t += 1
-        #if t == 40:
-        #    break  
-        if sink.countReceived == nitems:
-           break 
-    if not gotError:
-        print('Done after %d ticks' % t)
-        sink.printAll()
-    else:
-        print('Failed at tick %d ' % t)      
-
 
 def test():
     nitems = 200
 
-    source = Source('source','warehouse', delay=1, generator = genNitems(nitems))
+    source = Source('source','warehouse', delay=3, generator = genNitems(nitems))
     c11 = Conveyor('c1.1', delay = 1, capacity =2)
     c12 = Conveyor('c1.2', delay = 1, capacity =3)
     c13 = Conveyor('c1.3', delay = 1, capacity =5)
@@ -85,22 +43,20 @@ def test():
     probe = Probe('all picked probe','',
         checkerPredicate = lambda load: load.pickingFinished())
 
-    ns01Pickers = 1
+    ns01Pickers = 4
     for i in range(ns01Pickers):
         s01.add_agent(PickingAgent(
             's01-%2.2dPicker'%(i+1),
-            's01-%2.2dPicker'%(i+1), 
             destination=c2, 
             delay=10,
             markWorkload= markAsPicked(['A']), 
             stopConveyor=False, 
             maxBlockedTime= 200))
 
-    ns02Pickers = 1
+    ns02Pickers = 2
     for i in range(ns02Pickers):
         s02.add_agent(PickingAgent(
             's02-%2.2dPicker'%(i+1),
-            's02-%2.2dPicker'%(i+1), 
             destination=c2, 
             delay=10,
             markWorkload= markAsPicked(['C']), 
@@ -132,15 +88,15 @@ def test():
             print(e)
             source.print()
             break
-        if t % 1000 == 0:
-            source.print()
+        #if t % 1000 == 0:
+        #    source.print()
         #source.print()
         t += 1
         if sink.countReceived == nitems:
             break 
     if not gotError:
         print('Done after %d ticks' % t)
-        sink.printAll()
+        #sink.printAll()
     else:
         print('Failed at tick %d ' % t)
 if __name__ == '__main__':
