834dee47cff13f0b5e92924417b4870a12bc825c
Vangel Paronov <vparonov@gmail.com>
2022-12-26 14:06:13 +0200

    checkpoint

--- state.py
+++ state.py
@@ -1,30 +1,32 @@
 from collections import defaultdict
 
 class State:
-    def __init__(self, nitems, ncomponents) -> None:
-        self.itemsState = defaultdict(lambda : -1)
-        self.componentsState = [0] * ncomponents
+    def __init__(self) -> None:
+        self.itemsState = defaultdict(lambda : '__buffer__')
+        self.componentsState= defaultdict(lambda : 0)
 
     def update(self, itemID, componentID):
         prevComponentID = self.itemsState[itemID]
-        if prevComponentID >= 0:
-            self.componentsState[prevComponentID] -= 1
+        self.componentsState[prevComponentID] -= 1
 
         self.itemsState[itemID] = componentID
         self.componentsState[componentID] += 1
 
     def get(self):
         return self.itemsState, self.componentsState
+
+
+#tests 
 if __name__ == '__main__':
-    s = State(5, 5)
+    s = State()
     print(s.get())
-    s.update(0, 0)
+    s.update(0, 'C1')
     print(s.get())
-    s.update(0, 1)
+    s.update(0, 'C2')
     print(s.get())
-    s.update(1, 0)
+    s.update(1, 'C1')
     print(s.get())
-    s.update(1, 1)
+    s.update(1, 'C2')
     print(s.get())
 
     itemsDict, componentsState = s.get()
